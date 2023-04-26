6bea6af4355f7d891e36cfc7775769d9d120ca3a
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-04-26 21:15:00 +0200

    added-lenda-table

--- src/main/java/com/example/knk_project/database/databaseCreation.sql
+++ src/main/java/com/example/knk_project/database/databaseCreation.sql
@@ -12,4 +12,14 @@ create table if not exists nxenesit(
     vend_lindja varchar(50),
     komuna varchar(50),
     shteti varchar(50)
+);
+
+create table if not exists lendet(
+	id integer not null auto_increment primary key,
+    emri varchar(50),
+    tekstet varchar(100),
+    autori varchar(100),
+    shtepia_botuese varchar(50),
+    viti_botimit year
+    
 )
