5a1e11f2a6e58d0dd9754eb0c22431c0d9b4a059
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-05-04 00:08:29 +0200

    added-sign-up-nxenesi-controller

--- src/main/java/com/example/knk_project/controllers/SignUpNxenesiController.java
+++ src/main/java/com/example/knk_project/controllers/SignUpNxenesiController.java
@@ -1,4 +1,41 @@
 package com.example.knk_project.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpNxenesiController {
+    @FXML
+    private TextField emriTextField;
+    @FXML
+    private TextField mbiemriTextField;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private DatePicker dateLindjaDatePicker;
+    @FXML
+    private PasswordField passwordPasswordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+    @FXML
+    private ComboBox<String> vendlinjdaComboBox;
+    @FXML
+    private ComboBox<String> komunaComboBox;
+    @FXML
+    private ComboBox<String> klasaComboBox;
+    @FXML
+    private TextField adresaTextField;
+    @FXML
+    private TextField emriPrinditTextField;
+    @FXML
+    private TextField mbiemriPrinditTextField;
+    @FXML
+    private TextField profesioniPrinditTextField;
+    @FXML
+    private TextField numriTelefonitPrinditTextField;
+    @FXML
+    private TextField emailPrinditTextField;
+
 }
