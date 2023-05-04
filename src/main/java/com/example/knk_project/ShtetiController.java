81cf70e88722a9302c0a7790eb058947a4aff7cd
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-05-04 21:53:37 +0200

    added-shto-shtetin-view

---
+++ src/main/java/com/example/knk_project/ShtetiController.java
@@ -0,0 +1,31 @@
+package com.example.knk_project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class ShtetiController {
+
+    @FXML
+    private Label AddState;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private VBox myVbox;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private CheckBox terms;
+
+    @FXML
+    private TextField writeState;
+
+}
