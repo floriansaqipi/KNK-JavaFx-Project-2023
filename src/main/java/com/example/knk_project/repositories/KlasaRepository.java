55cbb36ba6f1c3b0f917ef50b4fe55ecb7814c4e
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-05-05 21:43:06 +0200

    added-klasa-repository

---
+++ src/main/java/com/example/knk_project/repositories/KlasaRepository.java
@@ -0,0 +1,23 @@
+package com.example.knk_project.repositories;
+
+import com.example.knk_project.repositories.interfaces.KlasaRepositoryInterface;
+import com.example.knk_project.services.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class KlasaRepository extends KlasaRepositoryInterface {
+    @Override
+    public void insert(CreateKlasaDto createKlasaDto) throws SQLException {
+        String sql = "INSERT INTO klasat(klasa, paralelja, viti) VALUES (?, ?, ?);";
+        Connection connection = ConnectionUtil.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1,createKlasaDto.getKlasa());
+        preparedStatement.setInt(2,createKlasaDto.getParalelja());
+        preparedStatement.setInt(3,createKlasaDto.getYear());
+
+        preparedStatement.executeUpdate();
+
+    }
+}
