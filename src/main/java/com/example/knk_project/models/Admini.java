356170cbc15b7e6dedd1157de706f6901929a761
Gentrit <gentikryeziu851@gmail.com>
2023-05-05 21:17:38 +0200

    add-admin-hotfix

--- src/main/java/com/example/knk_project/models/Admini.java
+++ src/main/java/com/example/knk_project/models/Admini.java
@@ -5,26 +5,17 @@ public class Admini {
     private String username;
     private String salt;
     private String saltedPassword;
-    private String emri;
-    private String mbiemri;
-    private String titulli;
 
     public Admini(
             int id,
             String username,
             String salt,
-            String saltedPassword,
-            String emri,
-            String mbiemri,
-            String titulli)
+            String saltedPassword)
     {
         this.id = id;
         this.username = username;
         this.salt = salt;
         this.saltedPassword = saltedPassword;
-        this.emri = emri;
-        this.mbiemri = mbiemri;
-        this.titulli = titulli;
     }
 
     public int getId() {
@@ -41,17 +32,5 @@ public class Admini {
 
     public String getSaltedPassword() {
         return saltedPassword;
-    }
-
-    public String getEmri() {
-        return emri;
-    }
-
-    public String getMbiemri() {
-        return mbiemri;
-    }
-
-    public String getTitulli() {
-        return titulli;
     }
 }
