cb0bb384cc6ef6dc47194fe5c2ef0403d2df53d8
erzamerovci <erzaamerovci@gmail.com>
2023-05-06 01:00:23 +0200

    View and controller for adding subject

---
+++ src/main/java/com/example/knk_project/controllers/addSubjectController.java
@@ -0,0 +1,37 @@
+package com.example.knk_project.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class addSubjectController implements Initializable {
+    @FXML
+    private ComboBox addSubjectComboBox;
+    @FXML
+    private ComboBox chooseProfessorComboBox;
+
+    void selectSubject(ActionEvent event) {
+    }
+    void selectProfessor(ActionEvent event) {
+    }
+    public void initialize() {
+
+    }
+
+
+    @FXML
+
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
+
+
+
+
+
+
