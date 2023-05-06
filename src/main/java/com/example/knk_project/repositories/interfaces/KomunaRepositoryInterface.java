5467d885bd275a877a35c5e345ac28e495c4146e
erzamerovci <erzaamerovci@gmail.com>
2023-05-06 18:28:43 +0200

    imported classes in repositories

--- src/main/java/com/example/knk_project/repositories/interfaces/KomunaRepositoryInterface.java
+++ src/main/java/com/example/knk_project/repositories/interfaces/KomunaRepositoryInterface.java
@@ -1,7 +1,11 @@
 package com.example.knk_project.repositories.interfaces;
 
+import com.example.knk_project.models.dto.CreateKomunaDto;
+
 import java.sql.SQLException;
 
 public interface KomunaRepositoryInterface {
-    void insert(CreateKomunatDto createKomunatDto) throws SQLException;
+
+
+    void insert(CreateKomunaDto createKomunaDto) throws SQLException;
 }
--- src/main/java/com/example/knk_project/repositories/interfaces/ShtetiRepositoryInterface.java
+++ src/main/java/com/example/knk_project/repositories/interfaces/ShtetiRepositoryInterface.java
@@ -1,7 +1,10 @@
 package com.example.knk_project.repositories.interfaces;
 
+import com.example.knk_project.models.dto.CreateShtetiDto;
+
 import java.sql.SQLException;
 
 public interface ShtetiRepositoryInterface {
     void insert(CreateShtetiDto createShtetiDto) throws SQLException;
+
 }
