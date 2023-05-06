61ad5c7f611ed9d5f2e06b005250280af03d0ded
FjollaAjeti <115341464+fjolla-ajeti@users.noreply.github.com>
2023-05-06 14:04:59 +0200

    added controller

--- src/main/java/com/example/knk_project/controllers/ProfesorKlasaController.java
+++ src/main/java/com/example/knk_project/controllers/ProfesorKlasaController.java
@@ -1,4 +1,26 @@
 package com.example.knk_project.controllers;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class ProfesorKlasaController {
+
+    @FXML
+    private ComboBox<String> klasaComboBox;
+
+    @FXML
+    private Label klasaLabel;
+
+    @FXML
+    private Label profesorKlasaLabel;
+
+    @FXML
+    private ComboBox<String> profesoriComboBox;
+
+    @FXML
+    private Label zgjidhProfesorinLabel;
+
+
 }
