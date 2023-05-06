e2f272636c4f4360f859874057a7b36a0d7f10da
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-05-06 17:37:42 +0200

    added-models

---
+++ src/main/java/com/example/knk_project/models/Lenda.java
@@ -0,0 +1,20 @@
+package com.example.knk_project.models;
+
+public class Lenda {
+
+   private int id;
+   private String emri;
+
+   public Lenda(int id, String emri) {
+      this.id = id;
+      this.emri = emri;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public String getEmri() {
+      return emri;
+   }
+}
