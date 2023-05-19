c3225a682209f6516bff28cacc69c417f42996e1
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-05-19 23:54:48 +0200

    added home-page-view

---
+++ src/main/java/com/example/knk_project/controllers/HomePageController.java
@@ -0,0 +1,21 @@
+package com.example.knk_project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.image.ImageView;
+
+public class HomePageController {
+    @FXML
+    private MenuButton SignUp;
+
+    @FXML
+    private MenuButton LogIn;
+
+    @FXML
+    private ImageView homePageLogo;
+
+    @FXML
+    private ImageView HomePageImage;
+
+
+}
