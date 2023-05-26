64439da85ba946660d0812e2e46e7b2593c2164d
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-05-26 10:28:57 +0200

    added-goBack-button-image

--- src/main/java/com/example/knk_project/controllers/HomePageController.java
+++ src/main/java/com/example/knk_project/controllers/HomePageController.java
@@ -7,17 +7,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.w3c.dom.events.MouseEvent;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HomePageController implements Initializable {
     @FXML
@@ -34,6 +35,8 @@ public class HomePageController implements Initializable {
 
     @FXML
     private MenuItem professorItem;
+    @FXML
+    private Button goBackButton;
 
 
     // Action event handler for the adminItem
@@ -74,11 +77,28 @@ public class HomePageController implements Initializable {
         }
     }
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
 
+    public void GoBack(javafx.scene.input.MouseEvent mouseEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/knk_project/homepage-view.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage
+            Stage stage = new Stage();
+            stage.setTitle("Homepage");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            // Close the current stage
+            Stage currentStage = (Stage) goBackButton.getScene().getWindow();
+            currentStage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ProfesorPageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
